54cd7c4d01f5c512e9b913d40a99a6021605c906
seifhamdy <shsabry@uofcanada.edu.eg>
2023-06-22 04:31:12 +0300

    improved exception handling

---
+++ client/src/config/apiConfig.ts
@@ -0,0 +1,13 @@
+type ApiUrls = {
+    [key: string]: string;
+  };
+
+  const API_URLS: ApiUrls = {
+    development: 'http://localhost:3000/api/v1',
+    production: 'https://production-api-url.com/api/v1',
+  };
+
+  export const getApiUrl = (): string => {
+    const env = process.env.NODE_ENV || 'development';
+    return API_URLS[env];
+  };
--- server/lib/exception_handler.rb
+++ server/lib/exception_handler.rb
@@ -2,32 +2,36 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      render json: { error: e.message }, status: :not_found
-    end
-
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      render json: { error: e.message }, status: :unprocessable_entity
-    end
-
-    rescue_from ExceptionHandler::MissingToken, with: :missing_token
-
-    rescue_from ExceptionHandler::InvalidToken do |e|
-      render json: { error: e.message }, status: :unauthorized
-    end
-
-    rescue_from ActionController::ParameterMissing do |e|
-      render json: { error: e.message }, status: :unprocessable_entity
-    end
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :handle_record_invalid
+    rescue_from ActionController::ParameterMissing, with: :handle_parameter_missing
+    rescue_from JWT::DecodeError, with: :handle_invalid_token
+    rescue_from ActionController::RoutingError, with: :handle_routing_error
   end
 
   private
 
-  def missing_token(error)
-    render json: { error: error.message }, status: :unprocessable_entity
+  def handle_not_found(exception)
+    render_error(exception.message, :not_found)
   end
 
-  class MissingToken < StandardError; end
+  def handle_record_invalid(exception)
+    render_error(exception.message, :unprocessable_entity)
+  end
 
-  class InvalidToken < StandardError; end
+  def handle_parameter_missing(exception)
+    render_error(exception.message, :unprocessable_entity)
+  end
+
+  def handle_invalid_token(exception)
+    render_error(exception.message, :unauthorized)
+  end
+
+  def handle_routing_error(exception)
+    render_error('Route not found', :not_found)
+  end
+
+  def render_error(message, status)
+    render json: { error: message }, status: status
+  end
 end
