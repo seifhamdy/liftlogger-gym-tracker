cdb3c0aeb459a67356854e5b11056cbdea1a6a87
seifhamdy <shsabry@uofcanada.edu.eg>
2023-06-24 13:43:38 +0300

    minor fix

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
-require "dotenv/load"
+require "dotenv/load" if (ENV['RUBY_ENV'] == "development" || ENV['RUBY_ENV'] == "test")
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
