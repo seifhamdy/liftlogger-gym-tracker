d493928856bc3b4c4b5a3d63c4041badec0a4713
seifhamdy <shsabry@uofcanada.edu.eg>
2023-06-24 13:23:36 +0300

    deployment prep

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,12 @@
 # exit on error
 set -o errexit
 
-bundle install
-# clean
+# Add build commands for front end
 rm -rf public
-# build
 npm install --prefix client && npm run build --prefix client
-# migrate
-bundle exec rake db:migrate
-# postbuild
 cp -a client/build/. public/
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
